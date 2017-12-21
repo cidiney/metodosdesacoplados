b05085eb151a122559ba301017448d1e5d89cbbb
cidiney <34337922+cidiney@users.noreply.github.com>
2017-12-21 07:52:26 -0200

    Add files via upload

--- main_desacoplado_algoritmo.m
+++ main_desacoplado_algoritmo.m
@@ -87,8 +87,4 @@ end
 
 
 %% FUNCOES
-function H = retornaH11(deltak, Vk, Y)
-H = [DtijDj(Vk(1), Vk(2), real(Y(1,2)), imag(Y(1,2)), 0, deltak(1)) 0 0;
-    0 DtijDj(Vk(3), Vk(1), real(Y(3,1)), imag(Y(3,1)), deltak(2), 0) 0;
-    DpiDdi(i,j, V, G, B, deltai, deltaj)
-    
+
